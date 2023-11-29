053e0fa4fd982ae45749d9affc0fbfafbbc83761
QM <qmiao@users.noreply.github.com>
2023-11-29 10:30:59 +0000

    Add test and implementation for Elf class

--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -16,21 +16,38 @@ public class TestElfShould {
 
     @Test
     void have_1000_total_calories_after_adding_1000() {
-       fail("Not yet implemented");
+        final Elf elf = new Elf();
+        elf.addCalories(1000);
+        assertThat(elf.getTotalCalories(), equalTo(1000));       
     }
 
     @Test
     void compare_return_positive_value_for_elf_with_more_total_calories() {
-        fail("Not yet implemented");
+        final Elf elf1 = givenAnElfWithCalories(1000);
+        final Elf elf2 = givenAnElfWithCalories(999);
+
+        assertThat(elf1.compareTo(elf2), greaterThan(0));
     }
 
     @Test
     void compare_zero_for_equal_elf() {
-        fail("Not yet implemented");
+        final Elf elf1 = givenAnElfWithCalories(1000);
+        final Elf elf2 = givenAnElfWithCalories(1000);
+
+        assertThat(elf1.compareTo(elf2), equalTo(0));
     }
 
     @Test
     void compare_return_negative_value_for_elf_with_less_total_calories() {
-        fail("Not yet implemented");
+        final Elf elf1 = givenAnElfWithCalories(999);
+        final Elf elf2 = givenAnElfWithCalories(1000);
+
+        assertThat(elf1.compareTo(elf2), lessThan(0));
+    }
+
+    private static Elf givenAnElfWithCalories(final int calories) {
+        final Elf elf = new Elf();
+        elf.addCalories(calories);
+        return elf;
     }
 }
